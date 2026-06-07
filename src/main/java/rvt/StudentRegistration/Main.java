395eb7a50058e7d3ffe4bdd89d3f4868ac6e04ac
24DP2ENati <a240584en@rvt.lv>
2026-06-07 23:22:25 +0300

    Delete src/main/java/rvt/StudentRegistration/Main.java

--- src/main/java/rvt/StudentRegistration/Main.java
+++
@@ -1,8 +0,0 @@
-package rvt.StudentRegistration;
-
-public class Main {
-    public static void main(String[] args) {
-        RegistrationInterface ui = new RegistrationInterface();
-        ui.start();
-    }
-}
